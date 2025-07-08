6693d5847abae547b3b52638037bce3949ed0408
Sean <seanmicheals@hotmail.co.uk>
2025-07-08 13:57:51 +0100

    Initial commit

---
+++ src/main/java/com/version1/movies_and_shows_backend/MoviesAndShowsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.version1.movies_and_shows_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAndShowsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAndShowsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/version1/movies_and_shows_backend/MoviesAndShowsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version1.movies_and_shows_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAndShowsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
