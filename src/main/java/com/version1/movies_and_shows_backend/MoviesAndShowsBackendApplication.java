395ac27d2efcaa10574cc04266cfa196a00c43f9
Sean <seanmicheals@hotmail.co.uk>
2025-07-09 10:45:10 +0100

    Database connection added

--- src/main/java/com/version1/movies_and_shows_backend/MoviesAndShowsBackendApplication.java
+++ src/main/java/com/version1/movies_and_shows_backend/MoviesAndShowsBackendApplication.java
@@ -2,7 +2,7 @@ package com.version1.movies_and_shows_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class MoviesAndShowsBackendApplication {
