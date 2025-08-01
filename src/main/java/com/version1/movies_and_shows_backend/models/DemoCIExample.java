c080908a1094c1afec4d98e0ed9d6d2dbce646eb
Hibrar <hibbahibrar00@gmail.com>
2025-08-01 20:16:16 +0100

    Added file with issues for CI/CD demo

---
+++ src/main/java/com/version1/movies_and_shows_backend/models/DemoCIExample.java
@@ -0,0 +1,23 @@
+package com.version1.movies_and_shows_backend.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class DemoCIExample {
+    private String studentname;
+    private int age;
+    private String course;
+    private String uninitialized;
+
+    public String getInfo() {
+        String result = null;
+        if (studentname.equals("John"))
+            result = "Welcome, John!";
+        }
+        return result;
+    }
+}
